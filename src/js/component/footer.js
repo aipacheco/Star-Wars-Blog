3b4e060c7fb667e23e373e15f5c4c59c5765c0d5
Ana Pacheco <kornetera@gmail.com>
2023-03-14 21:20:27 +0000

    first commit

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+//import sableder from '../../img/sableder.png';
+import sableizq from '../../img/sableizq.png';
+
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
+		<img src={sableizq} alt="sable" className="sable"/>
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
 		</p>
 	</footer>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../../img/logo.png';
+import "../../styles/index.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+	
+		<nav className="navbar navbar-light mb-3">
+			<img src={logo} alt="Logo" className="logo"/>
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">favoritos</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
+	
 		</nav>
 	);
 };
