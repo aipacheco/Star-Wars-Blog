375c012e5123157163005c5cf564c37440a1191f
Ana Pacheco <kornetera@gmail.com>
2023-03-14 22:56:04 +0000

    pruebas

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/card.css";
 
 const Card = () =>{
 return (
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../../styles/home.css";
 import Card from "../component/card.jsx";
+import Carrusel from "../component/carrusel.jsx"
 
 export const Home = () => (
 	<div className="text-center mt-5" id="home">
+		<Carrusel/>
 		<Card />
 	</div>
 );
