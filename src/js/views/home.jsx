e35b27ffb9c60b54d628c8554b87c0bc15f21a9c
Ana Pacheco <kornetera@gmail.com>
2023-03-20 14:10:35 +0000

    spinner

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -5,12 +5,14 @@ import {getData} from "../service/index.js";
 import {useEffect, useState} from "react";
 
 export const Home = () => {
+  const [loading, setLoading] = useState(false)
   const [peopleList, setPeopleList] = useState([]);
   const [vehiclesList, setVehiclesList] = useState([]);
   const [planetsList, setPlanetsList] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
+      setLoading(true)
       const people = await getData("people", "");
       setPeopleList(people.results);
 
@@ -19,11 +21,21 @@ export const Home = () => {
 
       const planets = await getData("planets", "");
       setPlanetsList(planets.results);
+      setLoading(false)
     }
     fetchData();
   }, []);
 
   return (
+<>
+
+{loading ? (
+   <div className="d-flex justify-content-center">
+   <div class="spinner-grow text-light" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+ </div>
+    ) : (
     <div className="container">
       <div className="carrusel">
         {peopleList.map((people, key) => (
@@ -66,5 +78,7 @@ export const Home = () => {
         ))}
       </div>
     </div>
-  );
+  )}
+  </>
+)
 };
